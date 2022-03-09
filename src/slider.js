b233e96272e7b47c530102fbf059b7130456ceda
murgovgram <murgovski@gmail.com>
2022-03-09 21:46:04 +0100

    update

--- src/slider.js
+++ src/slider.js
@@ -3,6 +3,7 @@ function nextSlide() {
   activeSlide.classList.remove('translate-x-0');
   activeSlide.classList.add('-translate-x-full');
 
+
   let nextSlide = activeSlide.nextElementSibling;
   nextSlide.classList.remove('translate-x-full');
   nextSlide.classList.add('translate-x-0');
